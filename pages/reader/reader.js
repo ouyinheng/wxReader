8f0b59d8e011b61ee371f3b8cc0221157a410b2b
ouyinheng <2675379992@qq.com>
2018-09-28 22:43:07 +0800

    '完善'

--- pages/reader/reader.js
+++ pages/reader/reader.js
@@ -2,29 +2,41 @@
 Page({
   data: {
     start: 0,
-    distance: 0
+    distance: 0,
+    y: 0
   },
   onLoad: function (options) {
 
   },
   touchstart(e) {
     let start = e.changedTouches[0].clientX;
+    let distance = this.data.distance == '-100%' ? '-100%' : 0
     this.setData({
-      start,distance: 0
+      start,distance
     })
   },
   touchmove(e) {
     let move = e.changedTouches[0].clientX;
-    let distance = this.data.start-move;
-    this.setData({
-      distance
-    })
+    let distance = move-this.data.start;
+    if(distance>0){
+      return;
+    }
+    distance = distance < -30 ? distance : 0
+    this.setData({ distance: distance+'px' })
   },
   touchend(e) {
     let end = e.changedTouches[0].clientX;
-    let distance = this.data.start - end;
+    let distance = end-this.data.start;
+    if(distance>-30){
+      return;
+    }
+    distance = distance<=-100 ? '-100%': 0;
+    this.setData({ distance })
+  },
+  goleft(){
+    console.log(this.data.distance)
     this.setData({
-      distance
+      distance: 0
     })
   }
 })
