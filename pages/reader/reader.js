8cdbd4dcac4ea377a71c61df4c9243fb35e24291
ouyinheng <2675379992@qq.com>
2019-02-12 17:22:52 +0800

    genxing

--- pages/reader/reader.js
+++ pages/reader/reader.js
@@ -3,12 +3,18 @@ Page({
   data: {
     start: 0,
     distance: 0,
-    y: 0
+    y: 0,
+    bool: false,
+    canmove: true
   },
   onLoad: function (options) {
 
   },
   touchstart(e) {
+    if(!this.data.canmove)return;
+    this.setData({
+      bool: false
+    })
     let start = e.changedTouches[0].clientX;
     let distance = this.data.distance == '-100%' ? '-100%' : 0
     this.setData({
@@ -16,6 +22,7 @@ Page({
     })
   },
   touchmove(e) {
+    if(!this.data.canmove)return;
     let move = e.changedTouches[0].clientX;
     let distance = move-this.data.start;
     if(distance>0){
@@ -25,18 +32,28 @@ Page({
     this.setData({ distance: distance+'px' })
   },
   touchend(e) {
+    if(!this.data.canmove)return;
+    this.setData({
+      bool: true
+    })
     let end = e.changedTouches[0].clientX;
     let distance = end-this.data.start;
     if(distance>-30){
       return;
     }
     distance = distance<=-100 ? '-100%': 0;
+    if(distance=='-100%'){
+      this.setData({
+        canmove: false
+      })
+    }
     this.setData({ distance })
   },
   goleft(){
     console.log(this.data.distance)
     this.setData({
-      distance: 0
+      distance: 0,
+      canmove: true
     })
   }
 })
--- pages/search/search.js
+++ pages/search/search.js
@@ -14,22 +14,26 @@ Page({
     inputVal: "",
     hasnext: true,
     isLoadmore:false,
-    showLoad: false
+    showLoad: false,
+    searchValue: '',
+    history: [],
+    auto: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   getListData(){
+    this.showLoading();
     var that = this;
-    let { page, limit, inputVal } = this.data;
+    let { page, limit, searchValue } = this.data;
     this.setData({
       page: page,
       limit: limit,
       isLoadmore: true,
       showLoad:true
     })
-    var url = `https://api.zhuishushenqi.com/book/fuzzy-search?query=${inputVal}&start=${page}&limit=${limit}`
+    var url = `https://api.zhuishushenqi.com/book/fuzzy-search?query=${searchValue}&start=${page}&limit=${limit}`
     if (this.data.hasnext) {
       wx.request({
         url: url,
@@ -41,6 +45,7 @@ Page({
             isLoadmore: false,
             page: page+7
           })
+          that.cancelLoading();
         }
       })
     }
@@ -81,9 +86,92 @@ Page({
     });
   },
   inputTyping: function (e) {
+    console.log(e.detail.value)
+    if(!e.detail.value){
+      this.setData({
+        list: [],
+        auto: []
+      })
+      return;
+    }
     this.setData({
-      inputVal: e.detail.value
+      searchValue: e.detail.value
     });
+    this.getAuto()
+  },
+  getAuto() {
+    let _this = this;
+    wx.request({
+      url: `http://api.zhuishushenqi.com/book/auto-complete?query=${this.data.searchValue}`,
+      success(res) {
+        _this.setData({
+          auto: res.data.keywords
+        })
+      }
+    })
+  },
+  searchbook() {
+    if(!this.data.searchValue)return;
+    let history = [];
+    history.push(this.data.searchValue)
+    let res = wx.getStorageSync('history')
+    if(!res) {
+      wx.setStorageSync('history', history)
+    } else {
+      history = history.concat(res)
+      if(history.length>10)history.splice(10,1)
+      history = Array.from(new Set(history))
+      wx.setStorageSync('history', history)
+    }
+    this.setData({
+      history,
+      page: 0
+    })
     this.getListData();
+  },
+  searchbooks(e) {
+    let item = e.target.dataset.item;
+    this.setData({
+      searchValue: item
+    })
+    this.searchbook();
+  },
+  toRead(e) {
+    let id = e.detail
+    wx.navigateTo({
+      url: '../reader/reader?id=' + id
+    })
+  },
+  onShow() {
+    let _this = this;
+    wx.getStorage({
+      key: 'history',
+      success(res) {
+        history = res.data;
+        _this.setData({
+          history
+        })
+      }
+    });
+  },
+  del(e) {
+    let index = e.target.dataset.id;
+    let data = this.data.history;
+    let _this = this;
+    data.splice(index,1);
+    wx.showModal({
+      title: '提示',
+      content: '确定删除吗',
+      success(res) {
+        if (res.confirm) {
+          _this.setData({
+            history: data
+          })
+          wx.setStorageSync('history', data)
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })
   }
 })
